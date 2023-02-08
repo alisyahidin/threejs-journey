97a4f6798abe522139ce93b5fc66bf88b7a840e3
Ali Syahidin <ali@fivejack.com>
2023-02-08 11:25:58 +0700

    Change bundler from webpack to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,ts}'],
+  content: [
+    './**/*.{html,css,ts}',
+  ],
   theme: {
     extend: {},
   },
