f0b75f2a1b1a3f034783d36d208ebd91c7ef3ac5
Ali Syahidin <ali@fivejack.com>
2023-04-05 14:38:49 +0700

    update lesson 16 part 1

--- lessons/16-shadow/main.ts
+++ lessons/16-shadow/main.ts
@@ -4,12 +4,10 @@ import GUI from 'lil-gui';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 class Scene extends THREE.Scene {
-  debugger: GUI = null;
+  debugger: GUI = new GUI();
   camera: THREE.PerspectiveCamera = null;
   renderer: THREE.WebGLRenderer = null;
   orbitals: OrbitControls = null;
-  lights: Array<THREE.Light> = [];
-  lightCount: number = 6;
   lightDistance: number = 7;
   width = window.innerWidth;
   height = window.innerHeight;
@@ -30,6 +28,8 @@ class Scene extends THREE.Scene {
     });
     this.renderer.setSize(this.width, this.height);
     this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.shadowMap.enabled = true
+    this.renderer.shadowMap.type = THREE.BasicShadowMap
 
     // add window resizing
     Scene.addWindowResizing(this.camera, this.renderer);
@@ -41,59 +41,132 @@ class Scene extends THREE.Scene {
     // Adds an origin-centered grid for visual reference
     if (addGridHelper) {
       this.add(new THREE.GridHelper(10, 10, 'red'));
-      this.add(new THREE.AxesHelper(3))
     }
 
     // set the background color
     this.background = new THREE.Color(0x000);
 
-    // create the lights
-    for (let i = 0; i < this.lightCount; i++) {
-      // Positions evenly in a circle pointed at the origin
-      const light = new THREE.PointLight(0xffffff, 1);
-      let lightX = this.lightDistance * Math.sin(Math.PI * 2 / this.lightCount * i);
-      let lightZ = this.lightDistance * Math.cos(Math.PI * 2 / this.lightCount * i);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.4)
+    this.add(ambientLight)
 
-      // Create a light
-      light.position.set(lightX, this.lightDistance / 2, lightZ)
-      light.lookAt(0, 0, 0)
+    // Shadow light
+    const light = new THREE.DirectionalLight(0xffffff, 0.3)
+    light.position.set(2, 2, -1)
+    light.castShadow = true
+    light.shadow.mapSize.set(1024, 1024)
+    light.shadow.camera.far = 14
+    light.shadow.radius = 5
+    this.add(light)
 
-      this.add(light);
+    const lightShadowHelper = new THREE.CameraHelper(light.shadow.camera)
+    this.add(lightShadowHelper)
 
-      this.lights.push(light);
-      // Visual helpers to indicate light positions
-      this.add(new THREE.PointLightHelper(light, .5, 0xff9900));
-    }
+    const spotLight = new THREE.SpotLight(0xffffff, 0.3)
+    spotLight.castShadow = true
+    spotLight.position.set(0, 2, -2)
+    spotLight.shadow.mapSize.set(1024 * 2, 1024 * 2)
+    spotLight.shadow.camera.fov = 30
+    spotLight.shadow.camera.far = 8
+    this.add(spotLight)
+
+    const spotLightHelper = new THREE.CameraHelper(spotLight.shadow.camera)
+    this.add(spotLightHelper)
+
+    const pointLight = new THREE.PointLight(0xffffff, 0.3)
+    pointLight.position.set(-3, 3, 3)
+    pointLight.castShadow = true
+    pointLight.shadow.mapSize.set(1024, 1024)
+    this.add(pointLight)
+
+    const pointLightHelper = new THREE.CameraHelper(pointLight.shadow.camera)
+    this.add(pointLightHelper)
 
     // Creates the geometry + materials
     let sphere = new THREE.Mesh(
-      new THREE.SphereGeometry(0.5, 16, 16),
-      new THREE.MeshStandardMaterial({ color: 0xff9900 })
+      new THREE.SphereGeometry(0.5, 12, 12),
+      new THREE.MeshStandardMaterial({ color: 0xf542d1, roughness: 0.4 })
     );
-    sphere.position.y = 1;
+    sphere.position.y = 0.5;
+    sphere.castShadow = true
     this.add(sphere);
 
     const plane = new THREE.Mesh(
       new THREE.PlaneGeometry(8, 8),
-      new THREE.MeshStandardMaterial()
+      new THREE.MeshStandardMaterial({ roughness: 0.4 })
     )
     plane.rotation.x = Math.PI * -0.5
+    plane.receiveShadow = true
     this.add(plane)
 
     // setup Debugger
     if (debug) {
-      this.debugger = new GUI();
       const lightGroup = this.debugger.addFolder('Lights');
-      for (let i = 0; i < this.lights.length; i++) {
-        lightGroup.add(this.lights[i], 'visible');
-      }
+      lightGroup.add(ambientLight, 'intensity', 0, 1).name('Ambient intensity')
+      lightGroup.add(light, 'intensity', 0, 1).name('Directional intensity')
+      lightGroup.add(light.position, 'x', -4, 4)
+      lightGroup.add(light.position, 'z', -4, 4)
+      lightGroup.add(light.position, 'y', 0, 8)
       lightGroup.open();
+
+      const lightShadowGroup = this.debugger.addFolder('Shadow');
+      const directionalGroup = lightShadowGroup.addFolder('Directional light')
+      directionalGroup.add(light, 'visible').name('Light')
+      directionalGroup.add(lightShadowHelper, 'visible').name('helper')
+
+      const spotlGroup = lightShadowGroup.addFolder('Spot light')
+      spotlGroup.add(spotLight, 'visible').name('Light')
+      spotlGroup.add(spotLightHelper, 'visible').name('helper')
+
+      const pointGroup = lightShadowGroup.addFolder('Point light')
+      pointGroup.add(pointLight, 'visible').name('Light')
+      pointGroup.add(pointLightHelper, 'visible').name('helper')
+
+      const params = {
+        basic: true,
+        pcf: false,
+        pcfSoft: false,
+        vsm: false,
+      }
+      const updateParams = (key: keyof typeof params) => {
+        for (let i in params) {
+          params[i as keyof typeof params] = false
+        }
+        params[key] = true
+        switch (key) {
+          case 'basic':
+            this.renderer.shadowMap.type = THREE.BasicShadowMap
+            break;
+          case 'pcf':
+            this.renderer.shadowMap.type = THREE.PCFShadowMap
+            break;
+          case 'pcfSoft':
+            this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
+            break;
+          case 'vsm':
+            this.renderer.shadowMap.type = THREE.VSMShadowMap
+            break;
+        }
+        sphere.material.needsUpdate = true
+        plane.material.needsUpdate = true
+      }
+      lightShadowGroup.add(params, 'basic').name('BasicShadowMap').listen().onChange(() => updateParams('basic'))
+      lightShadowGroup.add(params, 'pcf').name('PCFShadowMap').listen().onChange(() => updateParams('pcf'))
+      lightShadowGroup.add(params, 'pcfSoft').name('PCFSoftShadowMap').listen().onChange(() => updateParams('pcfSoft'))
+      lightShadowGroup.add(params, 'vsm').name('VSMShadowMap').listen().onChange(() => updateParams('vsm'))
+      lightShadowGroup.add(light.shadow, 'radius', 0, 7)
+
+
+      const meshGroup = this.debugger.addFolder('Objects');
+      meshGroup.add(sphere.position, 'x', -2, 2).name('Sphere X')
+      meshGroup.add(sphere.position, 'z', -2, 2).name('Sphere Z')
+      meshGroup.add(sphere.position, 'y', -2, 2).name('Sphere Y')
+      meshGroup.close();
 
       // Add camera to debugger
       const cameraGroup = this.debugger.addFolder('Camera');
       cameraGroup.add(this.camera, 'fov', 20, 80);
       cameraGroup.add(this.camera, 'zoom', 0, 1)
-      cameraGroup.open();
+      cameraGroup.close();
     }
   }
 
