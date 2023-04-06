1fd04407745c30ec48827a8a4057f5b434e2ffb7
Ali Syahidin <ali@fivejack.com>
2023-04-06 13:01:13 +0700

    update circular trigonometri function

--- circular.ts
+++ circular.ts
@@ -1,16 +1,43 @@
+import * as THREE from 'three'
+
+const getRadian = (angle: number) => angle * Math.PI / 180
+
 const app = document.getElementById('app');
 app?.classList.add('[&>*:nth-child(odd)]:bg-purple-400', '[&>*:nth-child(even)]:bg-blue-300')
 
-const halfRadius = 300;
+const clock = new THREE.Clock()
 
-for (let i = 0; i < 44; i++) {
-  const dot = document.createElement('div');
-  dot.className = 'h-6 w-6 absolute inset-0 m-auto rounded-lg'
+const dots: HTMLDivElement[] = [];
 
-  const x = Math.sin(i + 1) * halfRadius;
-  const y = Math.cos(i + 1) * halfRadius;
-
-  dot.style.transform = `translate(${x}px, ${y}px)`
-
-  app?.appendChild(dot);
+const params = {
+  radius: 300,
+  length: 12,
 }
+
+for (let i = 0; i < params.length; i++) {
+  const radian = getRadian(360 / params.length * i)
+
+  dots[i] = document.createElement('div');
+  dots[i].className = 'h-6 w-6 absolute inset-0 m-auto rounded-lg';
+
+  const x = Math.cos(radian) * params.radius;
+  const y = Math.sin(radian) * params.radius;
+  dots[i].style.transform = `translate(${x}px, ${y}px)`
+
+  app?.appendChild(dots[i]);
+}
+
+const loop = () => {
+  const elapsedTime = clock.getElapsedTime();
+  for (let i = 0; i < params.length; i++) {
+    const radian = getRadian(360 / params.length * i)
+
+    const x = Math.cos(radian + (elapsedTime * 0.3)) * params.radius;
+    const y = Math.sin(radian + (elapsedTime * 0.3)) * params.radius;
+
+    dots[i].style.transform = `translate(${x}px, ${y}px)`
+  }
+  requestAnimationFrame(loop)
+}
+
+loop()
