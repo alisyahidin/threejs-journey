028f0acd57fd04ef64bd7215e136ac43a66d339f
Ali Syahidin <ali@fivejack.com>
2023-04-08 02:58:52 +0700

    add spin in circular

--- playground/circular/wheel.ts
+++ playground/circular/wheel.ts
@@ -1,42 +1,48 @@
 import * as paper from 'paper';
 
-const canvas = document.getElementById('wheel') as HTMLCanvasElement
+const canvas = document.getElementById('wheel')!;
 
 paper.setup('wheel');
 paper.view.autoUpdate = true
 
-const radius = 200
-const gray = new paper.Color(0.6, 0.6, 0.6)
-const blue = new paper.Color(100 / 255, 152 / 255, 237 / 255)
+const radius = 400
 
-new paper.Path.Circle({
-  center: paper.view.center,
-  radius: radius,
-  strokeColor: blue,
-  strokeWidth: 2,
-});
+const rad = (angle: number) => (angle * Math.PI / 180)
 
-const pointer = new paper.Path.Line(paper.view.center, new paper.Point(paper.view.center.x + radius, paper.view.center.y))
-pointer.strokeColor = gray;
-pointer.strokeWidth = 2;
-
-// ---------
-
-
-const circleFull = 12
+const getAngle = (angle: number) => new paper.Point(
+  paper.view.center.x + (Math.cos(rad(angle)) * radius),
+  paper.view.center.y - (Math.sin(rad(angle)) * radius)
+)
 
 const wheel = new paper.Group()
 
-for (let i = 0; i < circleFull; i++) {
-  const seg = new paper.Path();
-  const circum = radius / 2 * 2 * Math.PI
-  const step = circum / circleFull
-  console.log(step)
-  seg.add([0, 0], [radius / 2, -step / 2 - 0.5], [radius / 2, step / 2 + 0.5])
-  seg.fillColor = blue
-  seg.fillColor.hue += 360 / circleFull * i
-  seg.rotate(360 / circleFull * i, [0, 0])
-  wheel.addChild(seg)
-}
+const foods = [
+  { title: 'Banana', color: 'red', probability: 0.3 },
+  { title: 'Mango', color: 'blue', probability: 0.4 },
+  { title: 'Pineapple', color: 'green', probability: 0.3 },
+  { title: 'Pineapple', color: 'yellow', probability: 0.3 },
+  { title: 'Pineapple', color: 'orange', probability: 0.3 },
+  { title: 'Pineapple', color: 'purple', probability: 0.3 },
+  // { title: 'Pineapple', color: 'black', probability: 0.3 },
+  // { title: 'Pineapple', color: 'white', probability: 0.3 },
+  // { title: 'Pineapple', color: 'pink', probability: 0.3 },
+]
 
-wheel.position = paper.view.center
+
+foods.forEach((food, i) => {
+  const rotation = 360 / foods.length;
+
+  const pie = new paper.Path.Arc({
+    from: getAngle(rotation * i),
+    through: getAngle((rotation * i) + (rotation / 2)),
+    to: getAngle(rotation * (i + 1)),
+    fillColor: food.color,
+  })
+
+  pie.add(new paper.Point(paper.view.center));
+  wheel.addChild(pie)
+})
+
+paper.view.onResize = () => {
+  wheel.position = paper.view.center
+}
